e931d1165e50c8b720ec960a3292263ad485b904
Aleksey <aleksey.dem@gmail.com>
2012-02-17 17:04:53 +0400

    keyword -> term

--- app/models/spree/suggestion.rb
+++ app/models/spree/suggestion.rb
@@ -1,11 +1,11 @@
 class Spree::Suggestion < ActiveRecord::Base
   validates :keywords, :presence => true
 
-  def self.relevant(keyword)
+  def self.relevant(term)
     config = Spree::Suggestion::Config
 
     select(:keywords).
-      where("keywords LIKE ? AND items_found != 0", keyword + '%').
+      where("keywords LIKE ? AND items_found != 0", term + '%').
       order("(#{config.count_weight}*count + #{config.items_found_weight}*items_found) DESC").
       limit(config.rows_from_db)
   end
