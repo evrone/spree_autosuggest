346c1d5cd41d3601f42b0013359666d5c166b434
Aleksey <aleksey.dem@gmail.com>
2012-02-17 10:53:06 +0400

    Removed useless

--- lib/generators/spree_autosuggest/install/install_generator.rb
+++ lib/generators/spree_autosuggest/install/install_generator.rb
@@ -4,12 +4,10 @@ module SpreeAutosuggest
 
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_autosuggest\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_autosuggest\n"
       end
 
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_autosuggest\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_autosuggest\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
