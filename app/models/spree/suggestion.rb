aae58ac0eea839d260fd8bea02a3e26e8524ba1a
Aleksey <aleksey.dem@gmail.com>
2012-02-17 10:56:35 +0400

    Limit by max

--- app/models/spree/suggestion.rb
+++ app/models/spree/suggestion.rb
@@ -1,8 +1,11 @@
 class Spree::Suggestion < ActiveRecord::Base
   validates :keywords, :presence => true
 
-  def self.relevant(keyword, count_weight=2, items_found_weight=1)
-    where("keywords LIKE ? AND items_found != 0", keyword + '%').order("(#{count_weight}*count + #{items_found_weight}*items_found) DESC").limit(5)
+  def self.relevant(keyword, max=5, count_weight=2, items_found_weight=1)
+    select(:keywords).
+      where("keywords LIKE ? AND items_found != 0", keyword + '%').
+      order("(#{count_weight}*count + #{items_found_weight}*items_found) DESC").
+      limit(max)
   end
 
 end
