5611b0c4c2666d5dc590a4cc1372f1f3594e8aad
Aleksey <aleksey.dem@gmail.com>
2012-02-24 12:39:39 +0400

    Save searches by @searcher.keywords instead params

--- app/controllers/base_controller_decorator.rb
+++ app/controllers/base_controller_decorator.rb
@@ -3,10 +3,10 @@ Spree::BaseController.class_eval do
 
 
   def save_search
-    suggest_on = Spree::Suggestion::Config.field
+    keywords = @searcher.try :keywords
 
-    if @products.present? and params[suggest_on]
-      query = Spree::Suggestion.find_or_initialize_by_keywords(params[suggest_on])
+    if @products.present? and keywords.present?
+      query = Spree::Suggestion.find_or_initialize_by_keywords(keywords)
 
       query.items_found = @products.size
       query.increment(:count)
