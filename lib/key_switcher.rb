84a842531ac18881e5a0243725ecf549511543b3
Aleksey <aleksey.dem@gmail.com>
2012-02-27 15:40:07 +0400

    Zero matches bug fixed

--- lib/key_switcher.rb
+++ lib/key_switcher.rb
@@ -6,7 +6,9 @@ module KeySwitcher
     ru = "йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ".split('')
     en = "qwertyuiop[]asdfghjkl;'zxcvbnm,.`QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>~".split('')
 
-    first_letter = str.match(/([^\W\d_])/)[0]
+    return str unless letters = str.match(/([^\W\d_])/)
+
+    first_letter = letters[0]
     mapping = first_letter.in?(ru) ? Hash[ru.zip(en)] : Hash[en.zip(ru)]
 
     str.split('').inject('') { |result, char| result << (mapping[char] || char) }
