a511a7ed9dd6a156492370fb9984b547ff5df3f3
Alexander Balashov <divineforest@gmail.com>
2015-07-22 21:19:01 +0300

    Add jquery UI autocomplete files in install generator

--- lib/generators/spree_autosuggest/install/install_generator.rb
+++ lib/generators/spree_autosuggest/install/install_generator.rb
@@ -3,7 +3,12 @@ module SpreeAutosuggest
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require store/spree_autosuggest\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js",
+          "//= require jquery-ui/autocomplete\n//= require frontend/spree_autosuggest\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require jquery-ui/autocomplete\n", before: /\*\//, verbose: true
       end
 
       def add_migrations
